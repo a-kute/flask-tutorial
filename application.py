f32bbd640661e325154f6f247cb3030686b7f3d9
Atharva Kute <70390979+a-kute@users.noreply.github.com>
2021-06-04 21:12:42 -0400

    Add files via upload

--- application.py
+++ application.py
@@ -1,61 +1,72 @@
 from flask import Flask, render_template, request
-from matplotlib import pyplot as plt
-import cv2
-#from mtcnn.mtcnn import MTCNN
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 
 
 
 application = Flask(__name__)
 
+
 @application.route('/')
 def hello_world():
+
     return render_template('before.html')
-# def draw_box(image_path, faces):
-#     image = plt.imread(image_path)
-#     plt.imshow(image)
-#     ax = plt.gca()
-#     for face in faces:
-#         x,y,w,h = face['box']
-#         face_border = Rectangle((x,y),w,h,fill=False,color='red')
-#         ax.add_patch(face_border)
-#
-#     plt.axis('off')
-#     plt.savefig('static/a1.jpg')
-#     plt.show()
-#
+
+
+emotion_model = Sequential()
+
+emotion_model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(48, 48, 1)))
+emotion_model.add(Conv2D(64, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Conv2D(128, kernel_size=(3, 3), activation='relu'))
+emotion_model.add(MaxPooling2D(pool_size=(2, 2)))
+emotion_model.add(Dropout(0.25))
+
+emotion_model.add(Flatten())
+emotion_model.add(Dense(1024, activation='relu'))
+emotion_model.add(Dropout(0.5))
+emotion_model.add(Dense(7, activation='softmax'))
+emotion_model.load_weights('static/emotion_model.h5')
+emotion_model.summary()
+if __name__ == '__main__':
+    application.run()
 
 
 @application.route('/uploader', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
+
         files = request.files.getlist("file")
-        count = 0
         for file in files:
-            if count==0:
-                file.filename="static/f1.jpg"
-            else:
-                file.filename="static/s1.jpg"
+            file.filename="static/s1.jpg"
             file.save(file.filename)
-            count+=1
 
 
+        img = keras.preprocessing.image.load_img('static/s1.jpg', target_size=(48, 48), grayscale=True)
 
-        # filename = secure_filename(file.filename)
-        # file.save(os.path.join(application.config['UPLOAD_FOLDER'], filename))
+        img_array = keras.preprocessing.image.img_to_array(img)
+        #plt.imshow(img_array)
+        #plt.show()
+        img_array = tf.expand_dims(img_array, 0)  # Create a batch
+        #print(img_array.shape)
+        #emotion_prediction = emotion_model.predict(img_array)
+        emotion_prediction = emotion_model(img_array)
 
+        maxindex = int(np.argmax(emotion_prediction))
+        print(maxindex)
+        emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
 
-        image_uno = plt.imread('static/f1.jpg')
-        image_dos = plt.imread('static/s1.jpg')
-        #draw_box('static/f1.jpg', MTCNN.detect_faces(image_uno))
-        #draw_box('static/s1.jpg', MTCNN.detect_faces(image_dos))
+        return render_template('after.html',data = emotion_dict[maxindex])
 
-
-        ##perform_predictions("static/f1.jpg","static/s1.jpg")
-        img3 = cv2.imread('static/first.jpg')
-        # cv2.imwrite('trial/afterI.jpg', img3)
-        # img2 = cv2.imread('trial/second.jpg')
-        # cv2.imwrite('trial/afterI2.jpg', img2)
+    else:
         return render_template('after.html')
-
 
 
